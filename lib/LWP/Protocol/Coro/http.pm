ffc47857150bf2c85cf5b5501f69d6cb6dcabf14
Eric Brine <ikegami@adaelis.com>
2016-05-09 15:47:12 -0400

    Useless

--- lib/LWP/Protocol/Coro/http.pm
+++ lib/LWP/Protocol/Coro/http.pm
@@ -134,7 +134,7 @@ sub request {
    }
 
    # Let LWP handle redirects and cookies.
-   my $guard = http_request(
+   http_request(
       $method => $url,
       headers => \%headers,
       %opts,
